2efc47a056ec95134ffb607fae05b5fdab80ce12
PRATHAMKARMARKAR <karmarkarpratham56@gmail.com>
2025-07-11 20:27:23 +0530

    first commit4

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <>
     <div className="navbar text-[#FD520F] text-2xl  flex  justify-center gap-48 items-center   ">
-          <div className="ml-30 flex -mt-12  justify-center gap-48 items-center  ">
+          <div className="ml-30 flex -mt-16  justify-center gap-48 items-center  ">
            <div className="text ml-10  w-30">About Us</div>
            <div className="text ml-10 w-30">Home</div>
            <div className="text  w-30">Contact Us</div></div>
